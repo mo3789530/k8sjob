87633dc8ab363f2bdb12a609caa2f686a1be25fe
mo3789530 <mo3789530@gmail.com>
2022-02-06 14:51:01 +0900

    update

--- src/k8sClient.py
+++ src/k8sClient.py
@@ -5,10 +5,14 @@ class k8sClient():
     def getPods(namespace):
         config.load_config()
         k8s_core = client.CoreV1Api()
-        pods = k8s_core.list_namespaced_pod(namespace=namespace)
+        pods = k8s_core.list_namespaced_pod('default')
+        k8s_core.read_namespaced_pod_status('default')
+        k8s_core.read_namespace_status('default')
         for item in pods.items:
             print(item.matadata.name)
-            print(item.status)
+            container_statuses = item.status.container_statuses
+            for status in container_statuses:
+                print(status.state)
 
 
     def restart(namespaces, pod):
