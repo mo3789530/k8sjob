e84b073f6cb9961c64902728e1c8483de3d1760e
mo3789530 <mo3789530@gmail.com>
2022-03-23 22:05:17 +0900

    restart deply from k8s sdk

--- src/k8sClient.py
+++ src/k8sClient.py
@@ -1,14 +1,13 @@
 from kubernetes import client, config
 import datetime
-
+import pytz
 
 class k8sClient():
-
     def get_config(isIncluster):
         if isIncluster == true:
             # https://github.com/kubernetes-client/python/blob/master/examples/in_cluster_config.py
             return config.load_incluster_config()
-        else 
+        else:
             # https://github.com/kubernetes-client/python/blob/master/examples/out_of_cluster_config.py
             return config.load_kube_config()
 
@@ -28,29 +27,45 @@ class k8sClient():
         k8s_core.delete_namespaced_pod(name=pod, namespace=namespaces)
 
 
-    def get_v1_apps():
-        v1_apps = client.AppsV1Api()
-        return v1_apps
-
     def get_deployments(namespace):
-        v1_apps = get_v1_apps()
-        deployment = v1_apps.list_namespaced_deployment(namespace)
-        print(deployment)
+        v1_apps = client.AppsV1Api()
+        deployments = v1_apps.list_namespaced_deployment(namespace)
+        return deployments
 
     def get_deployment(namespace, name):
-        pass
+        v1_apps = client.AppsV1Api()
+        deployment = v1_app.list_namespaced_deployment(namespace)
+        for d in deployment.items:
+            if name == d.metadata.name:
+                return d
+        raise Exception("Not found exception")
 
-
-    def restart_deployment(namespace, name, deployment):
-        v1_apps = get_v1_apps()
+    def restart_deployment(namespace, name):
+        v1_apps = client.AppsV1Api()
+        deployment = get_deployment(namespace, name)
         deployment.spec.template.metadata.annotations = {
-            "kubectl.kubernetes.io/restartedAt": datetime.datetime.utcnow()
-            .replace(tzinfo=pytz.UTC)
-            .isoformat()
+            "kubectl.kubernetes.io/restartedAt": datetime.datetime.utcnow().replace(tzinfo=pytz.UTC).isoformat()
         }
+        deployment.api_version = "apps/v1"
+        deployment.kind = "Deployment"
         try:
             v1_apps.patch_namespaced_deployment(name, namespace, deployment, pretty='true')
         except ApiException as e:
             print("Exception when calling AppsV1Api->patch_namespaced_deployment %s\n" % e)
+            raise e
 
-
+    def restart_deployment_all(namespace):
+        v1_apps = get_v1_apps()
+        deployments = get_deployments()
+        for d in deployment.items:
+            d.spec.template.metadata.annotations = {
+                "kubectl.kubernetes.io/restartedAt": datetime.datetime.utcnow().replace(tzinfo=pytz.UTC).isoformat()
+            }
+            d.api_version = "apps/v1"
+            d.kind = "Deployment"
+            try:
+                v1_apps.patch_namespaced_deployment(d.metadata.name, namespace, d, pretty='true')
+            except ApiException as e:
+                print("Exception when calling AppsV1Api->patch_namespaced_deployment %s\n" % e)
+                raise e
+    
